8d9b3ffffb028e1ec8bef40ec516aabfaa6e9d45
Deceit-87 <manuelfreund318@gmail.com>
2022-03-24 20:19:17 +0100

    mainform complete

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,44 @@
 <template>
   <div id="app">
-   <Header/>
-   <Form/>
-   <Footer/>
+    <Header />
+    <Form />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/MainHeader.vue'
-import Footer from './components/MainFooter.vue'
-import Form from './components/MainForm.vue'
+import Header from "./components/MainHeader.vue";
+import Footer from "./components/MainFooter.vue";
+import Form from "./components/MainForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
     Header,
     Form,
     Footer,
-    
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
 :root {
-       
-  --text-xs:#7f9ea8;
-  --main-green: #85b680 ;
-  --over-green: #79b27b ;
+
+  --text-xs: #7f9ea8;
+  --main-green: #85b680;
+  --over-green: #79b27b;
+  --lightGrey: #e5e8eb;
+  --darkGrey: #484848;
 }
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
-ul,ol{
+ul,
+ol {
   list-style: none;
-  a{
+  a {
     color: currentColor;
   }
 }
@@ -47,50 +48,46 @@ ul,ol{
   -moz-osx-font-smoothing: grayscale;
   min-height: 100vh;
   min-width: 1920px;
-  font-size: 1.5 rem ;
-  
+  font-size: 1.5 rem;
 }
-  .nav-bar,
-  .green_button {
-    font-size: 16px;
-    font-weight: bold;
-    text-transform: uppercase;
-  }
+.nav-bar,
 .green_button {
-      color: white;
-      background-color: var(--main-green);
-      padding: 10px 20px;
-      border-radius: 29px;
-      border: 2px solid transparent;
-      text-align: center;
-      min-width: 145px;
-      height: 46px;
-      display: block;
-      &:hover{
-        background-color:var(--over-green)
-      }
-    }
-.container-fluid{
-  min-width: 1920px
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+.green_button {
+  color: white;
+  background-color: var(--main-green);
+  padding: 10px 20px;
+  border-radius: 29px;
+  border: 2px solid transparent;
+  text-align: center;
+  min-width: 145px;
+  height: 46px;
+  display: block;
+  &:hover {
+    background-color: var(--over-green);
+  }
+}
+.container-fluid {
+  min-width: 1920px;
 }
 .container {
   width: 1180px;
   margin: 0 auto;
   display: flex;
 }
-.container-half{
-  width: 50%;
+.container-half {
+  width: 41%;
+  margin-right: 5%;
 }
-.flex{
+.flex {
   display: flex;
 }
 #logo {
-        
-
-        img {
-          width: 260px;
+  img {
+    width: 260px;
+  }
 }
-}
-
-
 </style>
--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -2,19 +2,22 @@
   <section class="form-container container-fluid">
     <div class="container">
       <div class="container-half">
-        <h2>Get Behind The Wheel</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices
-          auctor sapien id cursus. Aliquam maximus turpis in vehicula semper.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices
-          auctor sapien id cursus. Aliquam maximus turpis in vehicula semper.
-          Fusce et ultrices libero, at suscipit lacus. Sed vitae fringilla nibh.
-          Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in
-          vehicula semper. Fusce et ultrices libero, at suscipit lacus. Sed
-          vitae fringilla nibh.
-        </p>
+        <div class="form-info">
+          <h3>Get Behind The Wheel</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula
+            semper.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula
+            semper. Fusce et ultrices libero, at suscipit lacus. Sed vitae
+            fringilla nibh. Nunc ultrices auctor sapien id cursus. Aliquam
+            maximus turpis in vehicula semper. Fusce et ultrices libero, at
+            suscipit lacus. Sed vitae fringilla nibh.
+          </p>
+        </div>
       </div>
       <div class="container-half">
         <div class="form-wrapper">
@@ -48,10 +51,10 @@
               placeholder="Location"
               minlength="3"
             />
-            <button type="submit" class="green_button" name="button">
-              request a callback
-            </button>
           </form>
+          <button type="submit" class="green_button" name="button">
+            request a callback
+          </button>
         </div>
       </div>
     </div>
@@ -70,11 +73,38 @@ export default {
 <style lang="scss" scoped>
 .form-container {
   position: relative;
-  min-height: 705px;
+  min-height: 673px;
   .container {
-    padding-top: 100px;
+    padding-top: 85px;
+
+    .form-info {
+      h3 {
+        font-size: 42px;
+        font-weight: 800;
+        padding: 0px 0 34px 0;
+        color: var(--darkGrey);
+      }
+
+      p {
+        font-size: 23px;
+        font-weight: 700;
+        line-height: 35px;
+        padding: 0 0 30px 0;
+        letter-spacing: -2px;
+        color: var(--text-xs);
+      }
+      p:last-child {
+        line-height: 32px;
+        letter-spacing: -1px;
+        font-size: 16px;
+        color: var(--text-xs);
+        width: 99%;
+      }
+    }
     .form-wrapper {
-      min-height: 659px;
+      padding: 40px;
+
+      min-height: 634px;
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -83,12 +113,47 @@ export default {
       top: -5%;
       background-color: #fff;
       border-radius: 10px;
+      p {
+        width: 92%;
+        text-align: center;
+        line-height: 30px;
+        padding-bottom: 36px;
+        color: var(--text-xs);
+        font-weight: 800;
+      }
+      h3 {
+        font-size: 30px;
+        font-weight: 801;
+        color: var(--main-green);
+        padding-bottom: 40px;
+      }
       form {
+        width: 100%;
+
         display: flex;
         flex-direction: column;
+
+        input {
+          margin-bottom: 28px;
+          height: 50px;
+          border-radius: 5px;
+          box-shadow: none;
+          border: 1px solid lightgray;
+          padding-left: 12px;
+          background-color: var(--lightGrey);
+          &::placeholder {
+            font-size: 12px;
+            color: rgba(71, 71, 71, 0.8);
+          }
+          &:focus {
+            outline: none;
+            box-shadow: none;
+            border-color: rgba(71, 71, 71, 0.4);
+          }
+        }
       }
     }
   }
 }
-</style>>
+</style>
 
